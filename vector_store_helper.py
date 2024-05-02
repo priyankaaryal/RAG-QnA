0a4ef9fdc4b99d140a737ebc288b9e883b0adf58
Priyanka Aryal <priyyankaaryal@gmail.com>
2024-05-02 17:39:43 -0500

    Added pysqlite3-binary dependency for linux systems

--- vector_store_helper.py
+++ vector_store_helper.py
@@ -1,5 +1,11 @@
 from typing import List, Tuple
 
+import sys
+
+if sys.platform == "linux":
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import chromadb
 
 
